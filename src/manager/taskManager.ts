e007df781bfd110b86452cbdc92c89b634d5f715
Felipe53 <felipeandresb97@gmail.com>
2022-02-03 11:54:54 -0300

    improve binding names to better match their purpose, i.e., better readability and organization

--- src/manager/taskManager.ts
+++ src/manager/taskManager.ts
@@ -1,20 +1,20 @@
-import buildLogin from '../steps/01_login'
-import buildGoToCourse from '../steps/02_course'
-import buildLessonsPlayer from '../steps/03_lessons'
+import buildLogin from '../tasks/01_login'
+import buildGoToCourse from '../tasks/02_course'
+import buildLessonsPlayer from '../tasks/03_lessons'
 import ProxyPage from '../types/ProxyPage'
 import delay from '../utils/delay'
 
 
 function buildTaskManager(page: ProxyPage) {
-  const actions: {(): Promise<void>}[] = []
+  const tasks: {(): Promise<void>}[] = []
 
-  actions.push(buildLogin(page))
-  actions.push(buildGoToCourse(page))
-  actions.push(buildLessonsPlayer(page))
+  tasks.push(buildLogin(page))
+  tasks.push(buildGoToCourse(page))
+  tasks.push(buildLessonsPlayer(page))
 
   async function run() {
-    for (const action of actions) {
-      await action()
+    for (const task of tasks) {
+      await task()
       await delay(1000)
     }
   }
